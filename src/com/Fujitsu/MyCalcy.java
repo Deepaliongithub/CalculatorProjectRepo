ba53311083f9cf37b0cf72bfe59867431f084156
Deepali <Deepaliatbanglore@gmail.com>
2022-08-02 15:44:46 +0530

    First Commit

---
+++ src/com/Fujitsu/MyCalcy.java
@@ -0,0 +1,11 @@
+package com.Fujitsu;
+
+public class MyCalcy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Write Switch case for 4 cases to add,multiply,divide,substraction		
+	}
+
+}
